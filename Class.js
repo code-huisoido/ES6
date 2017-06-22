e3afe94277eb72824dd32f4e003af1204a37b9f0
huisoido <584331125@qq.com>
2017-06-22 16:01:37 +0800

    docs: ES6 class写法

---
+++ Class.js
@@ -0,0 +1,21 @@
+class Animal{
+    constructor(){
+        this.type = 'animal';
+    }
+    say(say){
+        console.log(this.type + ' says ' + say);
+    }
+}
+
+let animal = new Animal();
+animal.say("hello");
+
+class Cat extends Animal{
+    constructor(){
+        super()
+        this.type = 'cat'
+    }
+}
+
+let cat = new Cat();
+cat.say("hello");
